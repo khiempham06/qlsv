6c124b2da59c4cee17c2199c16ca995b756a0b6e
unknown <khienph@yahoo.com>
2026-03-20 01:22:54 +0700

    fix

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -1,1 +1,14 @@
-<?php namespace App\Models; use Illuminate\Database\Eloquent\Model; class Submission extends Model { protected $fillable = ["assignment_id", "student_id", "file_path"]; public function student() { return $this->belongsTo(User::class, "student_id"); } }
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Submission extends Model
+{
+    protected $fillable = ["assignment_id", "student_id", "file_path"];
+    public function student()
+    {
+        return $this->belongsTo(User::class, "student_id");
+    }
+}
